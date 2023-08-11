4d61c9988f6e19ad38ecd78c949cb847e9e692bf
Antonio Saborido <antonio.saborido01@gmail.com>
2023-08-11 19:38:22 +0200

    Cors habilitadas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors');
+app.use(cors()); // This enables CORS for all routes
 const port = process.env.PORT || 8082;
 
 
@@ -10,16 +11,12 @@ app.listen(port, () => {
     console.log(`Server ready at port ${port}`);
 });
 
-
 var Datastore = require('nedb'),    
 db_tennis = new Datastore();
 db_tennis2 = new Datastore();
 
-const tennis_API = require("./src/tennis.js");
+const tennis_API = require("./src/back/tennis/tennis.js");
 tennis_API.register(app,db_tennis);
 
-const tennis_APIv2 = require("./src/tennisv2.js");
+const tennis_APIv2 = require("./src/back/tennis/tennisv2.js");
 tennis_APIv2.register(app,db_tennis2);
-
-const twitch = require("./src/tennistwitch.js");
-twitch.register(app);
